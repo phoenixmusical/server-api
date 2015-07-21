6b5c1390d6b0f1169fc7a96fe2e0ac5fb565e837
Frédéric Gascon <frederic.gascon@gmail.com>
2015-07-21 00:55:12 +0000

    adapted to work on c9.io

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var debug = require('debug')('phoenixmusical:api');
 var server = require('./lib/server');
 require('./lib/db');
 
-server.listen(config.require('http_port'), config.get('http_host', '0.0.0.0'), function(){
+server.listen(config.get('http_port', process.env.PORT), config.get('http_host', process.env.IP||'0.0.0.0'), function(){
 	debug("%s listening at %s", server.name, server.url);
 });
