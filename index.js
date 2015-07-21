ab2cc3a329757ef0037948f8a3cd674082ec9069
Frédéric Gascon <frederic.gascon@gmail.com>
2015-07-21 01:15:53 +0000

    moved db connection to index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var config = require('s-conf');
+var mongoose = require('mongoose');
 var debug = require('debug')('phoenixmusical:api');
 var server = require('./lib/server');
-require('./lib/db');
+
+mongoose.connect(config.require('db'));
 
 server.listen(config.get('http_port', process.env.PORT), config.get('http_host', process.env.IP||'0.0.0.0'), function(){
 	debug("%s listening at %s", server.name, server.url);
